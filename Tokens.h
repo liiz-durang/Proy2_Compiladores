8ac721660a0a0bafd4047988816100302ff5562a
lizduran <lizeth.durang@hotmail.com>
2021-11-18 21:59:02 -0600

    AnalizadorLexico con Flexer

--- Tokens.h
+++ Tokens.h
@@ -4,27 +4,48 @@
 /*Valores que nos retorna el Lexer al encontrar el token*/
 namespace COMP{
 
-    const int INT       = 1;
-    const int FLOAT     = 2;
-    const int ELSE      = 3;
-    const int IF        = 4;
-    const int WHILE     = 5;
+    const int CHAR      = 1; 
+    const int INT       = 2;
+    const int FLOAT     = 3;
+    const int DOUBLE    = 4;
+    const int STRUCT    = 5;
+    const int VOID      = 6;
+    const int IF        = 7;
+    const int ELSE      = 8;
+    const int WHILE     = 9;
+    const int DO        = 10;
+    const int PRINT     = 11;
+    const int SCAN      = 12;
+    const int BREAK     = 13;
+    const int RETURN    = 14;
 
-    const int MAS       = 6;
-    const int MENOS     = 7;
-    const int MUL       = 8;
-    const int DIV       = 9;
-    const int ASIG      = 10;
-    const int LPAR      = 11;
-    const int RPAR      = 12;
-    const int ID        = 13; 
-    const int PYC       = 14;
-    const int COMA      = 15;
-    const int NUMERO    = 16; //Entero o flotante
-    const int ESP       = 17;
+    const int LKEY      = 15;
+    const int RKEY      = 16;
+    const int LPAR      = 17;
+    const int RPAR      = 18;
+    const int MAS       = 19;
+    const int MENOS     = 20;
+    const int MUL       = 21;
+    const int DIV       = 22;
+    const int OR        = 23;
+    const int MENQ      = 24;
+    const int MAYQ      = 25;
+    const int IGUAL     = 26;
+    const int DIF       = 27;
+    const int MENI      = 28;
+    const int MAYI      = 29;
+    const int ASIG      = 30;
 
-    
+    const int PYC       = 31;
+    const int COMA      = 32;
+    const int PUNTO     = 33;
 
+    const int ESP       = 34;
+
+    const int NUMERO    = 35; //Entero o flotante o double
+    const int ID        = 36; 
+
+    const int CADENA    = 37; //cadena o caracter
 
 }
 #endif
--- lexer.ll
+++ lexer.ll
@@ -16,30 +16,61 @@
 
 
 ENTERO [0-9]+
-FLOTANTE [0-9]*.[0-9]+([Ee]([+-])?[0-9]+)? 
+FLOTANTE ([0-9])*.([0-9])+([Ee]([+-])?([0-9])+)?[fF]
+DOUBLE ([0-9])*.([0-9])+([Ee]([+-])?([0-9])+)?[dD]?
 ID [a-zA-Z][A-Za-z0-9_]*
-ESP [ \t\n\r]
+ESP [ \t\n\r]+
+CADENA  \"([\x20-\x21\x23-\xFE])*\”
+CARACTER \’[\x20-\x21\x23-\xFE \t\n\r]\’
 
 %%
+"char"		{return CHAR;}
+"int"		{return INT;}
+"float"		{return FLOAT;}
+"double" 	{return DOUBLE;}
+"struct"	{return STRUCT;}
+"void"		{return VOID;}
 
+"if"		{return IF;}
+"else"		{return ELSE;}
+"while"		{return WHILE;}
+"do"		{return DO;}
+"print"		{return PRINT;}
+"scan"		{RETURN SCAN;}
+"break"		{return BREAK;}
+"return"	{return RETURN;}
+
+"{"			{return LKEY;}
+"}" 		{return RKEY;}
+"(" 		{return lpar;}
+")" 		{return rpar;}
 "+"			{return MAS;}
 "-"			{return MENOS;}
 "*"			{return MUL;}
 "/"			{return DIV;}
+"&&" 		{return OR;}
+"<" 		{return MENQ;}
+">" 		{return MAYQ;}
+"=="		{return IGUAL;}
+"!="		{return DIF;}
+"<=" 		{return MENI;}
+">=" 		{return MAYI;}
 "="			{return ASIG;}
-"("			{return LPAR;}
-")"			{return RPAR;}
-"int"		{return INT;}
-"float"		{return FLOAT;}
-"if"		{return IF;}
-"else"		{return ELSE;}
-"while"		{return WHILE;}
+
 ";"			{return PYC;}
 ","			{return COMA;}
+"." 		{return PUNTO;}
+{ESP}		{}
+
 {ENTERO}	{return NUMERO;}
 {FLOTANTE}	{return NUMERO;}
+{DOUBLE}	{return NUMERO;}
+
 {ID}		{return ID;}
-{ESP}		{}
+
+{CADENA} 	{return CADENA;}
+{CARACTER}	{return CARACTER;}
+
 
 .		{cout<<"ERROR LEXICO " << yytext << endl;}
 
